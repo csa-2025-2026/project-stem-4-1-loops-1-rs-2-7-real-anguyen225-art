2e4f88b35fb8f1ed61b1a89535ee07212bdd1979
anguyen225-art <anguyen225@student.hbuhsd.edu>
2025-10-17 21:46:24 +0000

    class

--- Main.java
+++ Main.java
@@ -4,6 +4,23 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter non-negative numbers to add. Enter -1 to stop");
+
+    int N = 0;
+    int sum = 0;
+    while (N != -1)
+    {
+      N = sc.nextInt();
+      sum += N;
+    }
+    sum++;
+    System.out.println("The sum is: " + sum);
+
+
+    
+
+
+    sc.close();
   }
 }
